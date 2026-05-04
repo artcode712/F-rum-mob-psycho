8d1f516a6fd9f374b437b95e06e5ab2ca5a15bf1
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-05-03 23:58:20 -0300

    add- fix um sistema de mensagens entre os fans

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let form = document.getElementById('formulario');
 
-form.addEventListener('submit', function(e){
-
+form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     let valido = true;
@@ -9,30 +8,55 @@ form.addEventListener('submit', function(e){
     let nome = document.getElementById("nome").value;
     let senha = document.getElementById("senha").value;
 
-    if(nome.trim() === ""){
+    if (nome.trim() === "") {
         alert('Campo usuário vazio');
         valido = false
     }
 
-    if(senha.trim() === ""){
+    if (senha.trim() === "") {
         alert('Campo senha vazio');
         valido = false
     }
 
-    if(nome.trim() === 'admin'){
+    if (nome.trim() === 'admin') {
         valido = true
-    }else{
-       alert('Usuário incorreto');
+    } else {
+        alert('Usuário incorreto');
         valido = false
     }
 
-    if(senha.trim() === '1234'){
+    if (senha.trim() === '1234') {
         valido = true
-    }else{
-       alert('Senha incorreta');
+    } else {
+        alert('Senha incorreta');
         valido = false
     }
 
-    if(valido){
+    if (valido) {
         window.location.href = "home.html";
+    }
+
+
+    // envio de mensagens do bate-papo na aba de conversas
+    function sendMessage() {
+
+        const input = document.getElementById("messageInput");
+        const chatBox = document.getElementById("chatBox");
+
+        if (!input || !chatBox) {
+            console.log("Erro: elementos não encontrados");
+            return;
+        }
+
+        if (input.value.trim() === "") return;
+
+        const message = document.createElement("p");
+
+        message.innerHTML = `<strong>@Você</strong> - ${input.value}`;
+
+        chatBox.appendChild(message);
+
+        input.value = "";
+
+        chatBox.scrollTop = chatBox.scrollHeight;
     }
