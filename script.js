3cc0a030004c006c4e6407cc4938f8a7233547d2
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-05-04 22:21:01 -0300

    fix - corrigi as funcionalidades de login e comentários dentro do JavaScript.

--- script.js
+++ script.js
@@ -1,62 +1,35 @@
-let form = document.getElementById('formulario');
+let form = document.getElementById('form-login');
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+if (form) {
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-    let valido = true;
+        let nome = document.getElementById("nome").value;
+        let senha = document.getElementById("senha").value;
 
-    let nome = document.getElementById("nome").value;
-    let senha = document.getElementById("senha").value;
-
-    if (nome.trim() === "") {
-        alert('Campo usuário vazio');
-        valido = false
-    }
-
-    if (senha.trim() === "") {
-        alert('Campo senha vazio');
-        valido = false
-    }
-
-    if (nome.trim() === 'admin') {
-        valido = true
-    } else {
-        alert('Usuário incorreto');
-        valido = false
-    }
-
-    if (senha.trim() === '1234') {
-        valido = true
-    } else {
-        alert('Senha incorreta');
-        valido = false
-    }
-
-    if (valido) {
-        window.location.href = "home.html";
-    }
-
+        if (nome === "admin" && senha === "1234") {
+            window.location.href = "home.html";
+        } else {
+            alert("Usuário ou senha incorretos");
+        }
+    });
+}
 
     // envio de mensagens do bate-papo na aba de conversas
-    function sendMessage() {
+   function sendMessage() {
+    
+    const input = document.getElementById("messageInput");
+    const chatBox = document.getElementById("chatBox");
 
-        const input = document.getElementById("messageInput");
-        const chatBox = document.getElementById("chatBox");
+    if (!input || !chatBox) return;
 
-        if (!input || !chatBox) {
-            console.log("Erro: elementos não encontrados");
-            return;
-        }
+    if (input.value.trim() === "") return;
 
-        if (input.value.trim() === "") return;
+    const message = document.createElement("p");
+    message.innerHTML = `<strong>@Você</strong> - ${input.value}`;
 
-        const message = document.createElement("p");
+    chatBox.appendChild(message);
 
-        message.innerHTML = `<strong>@Você</strong> - ${input.value}`;
-
-        chatBox.appendChild(message);
-
-        input.value = "";
-
-        chatBox.scrollTop = chatBox.scrollHeight;
-    }
+    input.value = "";
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
